2f585bab75846a58688eba8939b57a2f0824d5dd
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-06-18 19:13:30 +0530

    Order Page

--- ecommerce-front/pages/_app.js
+++ ecommerce-front/pages/_app.js
@@ -1,5 +1,5 @@
-import {createGlobalStyle} from "styled-components";
-import {CartContextProvider} from "@/components/CartContext";
+import { createGlobalStyle } from "styled-components";
+import { CartContextProvider } from "@/components/CartContext";
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
@@ -15,9 +15,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
-      <CartContextProvider>
-        <Component {...pageProps} />
-      </CartContextProvider>
+        <CartContextProvider>
+          <Component {...pageProps} />
+        </CartContextProvider>
     </>
   );
 }
